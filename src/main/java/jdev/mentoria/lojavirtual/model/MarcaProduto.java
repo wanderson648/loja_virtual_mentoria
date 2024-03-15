aae8b47c17428042d5bfd8686fb6250fc8db599c
wanderson648 <testepedroca@gmail.com>
2024-03-14 23:39:14 -0300

    criado classe marca produto

---
+++ src/main/java/jdev/mentoria/lojavirtual/model/MarcaProduto.java
@@ -0,0 +1,22 @@
+package jdev.mentoria.lojavirtual.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+    private String nomeDescricao;
+}
